c0535b4ae804b9ff3a66e90e4c1248267d7f307f
jarble <jarble@gmail.com>
2021-09-25 12:50:53 -0500

    Update php_functions.go

--- php_to_go/php_functions.go
+++ php_to_go/php_functions.go
@@ -15,24 +15,29 @@ func explode(s string) string {
    //to do
 }
 
-func strpos(haystack string,needle string) {
-	return strings.index(haystack,needle)
+func strpos(haystack string,needle string) int {
+	return strings.Index(haystack,needle)
 }
 
-func strlen(s string){
+func strlen(s string) int{
 	return len(s)
 }
 
-func str_starts_with(haystack string, needle string) string{
-	return strings.hasPrefix(haystack,needle)
+func str_starts_with(haystack string, needle string) bool{
+	return strings.HasPrefix(haystack,needle)
 }
 
-func str_ends_with(haystack string, needle string) string{
-	return strings.hasSuffix(haystack,needle)
+func str_ends_with(haystack string, needle string) bool{
+	return strings.HasSuffix(haystack,needle)
 }
 
-func str_repeat(string str,times int){
-	return strings.repeat(str,times)
+func str_repeat(str string,times int) string{
+	return strings.Repeat(str,times)
+}
+
+//using generics
+func array_push[T any](a []T, b ...T) []T {
+	return append(a,b...)
 }
 
 //using generics
